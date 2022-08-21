75655938dda53f932d96a405ad9aac09bc946a23
jordanflynn1 <93089339+jordanflynn1@users.noreply.github.com>
2022-08-21 21:51:45 +0100

    commit

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Jordan Flynn';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'IT Managment Student';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
